e910014a8e87bb28c4af8c27b7b1b2db30eca8b8
Mykola <612284@gmail.com>
2022-03-19 23:27:15 +0200

    update photos

--- app.py
+++ app.py
@@ -3,16 +3,11 @@ import random
 app = Flask(__name__)
 # list of cat images
 images = [
-"https://loremflickr.com/cache/resized/65535_51675954859_dd93e58e9a_c_600_533_nofilter.jpg",
-"https://loremflickr.com/cache/resized/65535_51513517723_506d2cb22d_c_600_533_nofilter.jpg",
-"https://loremflickr.com/cache/resized/65535_51891814507_71dff930a2_c_600_533_nofilter.jpg",
-"https://loremflickr.com/cache/resized/65535_51420555879_6b66497824_b_600_533_nofilter.jpg",
-"https://loremflickr.com/cache/resized/65535_51815831292_9d722d6a24_c_600_533_nofilter.jpg",
-"https://loremflickr.com/cache/resized/65535_51538180961_147cd85f8c_c_600_533_nofilter.jpg",
-"https://loremflickr.com/cache/resized/65535_51564349748_2ceac19a11_b_600_533_nofilter.jpg",
-"https://loremflickr.com/cache/resized/65535_51503961890_4183f391c2_b_600_533_nofilter.jpg",
-"https://loremflickr.com/cache/resized/65535_51427824421_8d91d4aee4_h_600_533_nofilter.jpg",
-"https://loremflickr.com/cache/resized/65535_51922208154_94957bf6a4_c_600_533_nofilter.jpg"
+"https://i.picsum.photos/id/136/4032/2272.jpg?hmac=8ygXp61m49P3x_uMkBih2sZHJwEaTLp5ZuOOVNE9qhU",
+"https://i.picsum.photos/id/1084/4579/3271.jpg?hmac=YblMazviSugJVfZsFPaFI_Vp6lBeQin62qpm8rxHruo",
+"https://i.picsum.photos/id/1074/5472/3648.jpg?hmac=w-Fbv9bl0KpEUgZugbsiGk3Y2-LGAuiLZOYsRk0zo4A",
+"https://i.picsum.photos/id/1055/5472/3648.jpg?hmac=1c293cGVlNouNQsjxT8y3nsYO-7qLCaOBEGvih0ibEU",
+"https://i.picsum.photos/id/1025/4951/3301.jpg?hmac=_aGh5AtoOChip_iaMo8ZvvytfEojcgqbCH7dzaz-H8Y"
 ]
 @app.route('/')
 def index():
