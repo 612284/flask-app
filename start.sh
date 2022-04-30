078f804e8b86543a4d08be1a4d2f2632bd9ee710
612284 <612284@gmail.com>
2022-04-30 21:33:22 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-echo "public ip: "  >> /usr/src/app/templates/index.html
-curl https://checkip.amazonaws.com >> /usr/src/app/templates/index.html
-echo "<br>" >> /usr/src/app/templates/index.html
-echo "private ip: "  >> /usr/src/app/templates/index.html
-curl curl http://169.254.169.254/latest/meta-data/local-ipv4  >> /usr/src/app/templates/index.html
-echo "<br>" >> /usr/src/app/templates/index.html
-echo "random string: "  >> /usr/src/app/templates/index.html
-head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 30 | head -n 1 >> /usr/src/app/templates/index.html
-echo "<br>" >> /usr/src/app/templates/index.html
+PUBLIC_IP=$(curl https://checkip.amazonaws.com)
+PRIVATE_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+RANDOM_STRING=$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 30 | head -n 1)
+echo "<div class="container"> public ip: $PUBLIC_IP<br> private ip: $PRIVATE_IP<br> random static string unique for each container: <br> $RANDOM_STRING </div>" >> /usr/src/app/templates/index.html
 python /usr/src/app/app.py
